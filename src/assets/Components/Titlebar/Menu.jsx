e0b96e078ec1b721707f7a7de8ecd38698cea687
xVoid <xvoid.dev@outlook.com>
2025-09-06 14:56:13 -0400

    Added menu toggle functionality, just need to animate it for smoother transitions.

--- src/assets/Components/Titlebar/Menu.jsx
+++ src/assets/Components/Titlebar/Menu.jsx
@@ -3,7 +3,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function Menu(props) {
   return (
-    <div className="w-12 h-12 rounded-lg border-1 border-solid border-sky-400 flex items-center justify-center">
+    <div onClick={props.toggleMenu} className={`w-12 h-12 rounded-lg border-1 border-solid border-sky-400 flex items-center justify-center ${props.isOpen ? "rotate-180" : ""} `}>
       <FontAwesomeIcon icon={props.icon} className="text-violet-400 text-2xl animate-pulse" />
     </div>
   )
--- src/assets/Components/Titlebar/TitleBar.jsx
+++ src/assets/Components/Titlebar/TitleBar.jsx
@@ -1,15 +1,19 @@
-import React from "react";
+import React, {useState} from "react";
 import Icon from './Icon'
 import Menu from "./Menu";
 
 function TitleBar() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <div className="w-full h-1/12 bg-linear-to-b from-slate-950 to-violet-950 border-b-solid border-b-zinc-800 border-b-3 flex items-center justify-between">
       <img className="h-full cursor-pointer" src="../../../../src/assets/Images/voidbloom-title.png" />
-      <div className="icon-container mr-4 flex items-center justify-start gap-2">
-        <Menu icon="fa-solid fa-caret-left" />
-        <Icon icon="fa-solid fa-calculator" />
-        <Icon icon="fa-solid fa-list" />
+      <div className="mr-4 flex items-center justify-start gap-2">
+        <Menu toggleMenu={() => setIsOpen(!isOpen)} icon="fa-solid fa-caret-left" isOpen={isOpen} />
+        <div className={`icon-container flex items-center justify-center gap-2 ${isOpen ? "w-auto" : "w-0 hidden"}`}>
+          <Icon icon="fa-solid fa-calculator" />
+          <Icon icon="fa-solid fa-list" />
+        </div>
       </div>
     </div >
   )
